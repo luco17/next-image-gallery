10988eb3c28a1dbb72cec9d241bc24e4643ab6d3
luco17 <lucodonoghue@gmail.com>
2022-05-22 20:52:25 +0100

    added API types

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,4 +1,9 @@
-export default async function handler(req: any, res: any) {
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.query.secret !== process.env.REVALIDATE_SECRET) {
     return res.status(401).json({ message: 'Invalid token' })
   }
